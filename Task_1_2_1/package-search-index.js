5fc3049581c58810868a068a1fd2e5907a841ea4
Yojik1-cpu <Yojik1-cpu@users.noreply.github.com>
2025-10-24 17:46:25 +0000

    Deploying to gh-pages from @ Yojik1-cpu/OOP@a22743f607550879a90cd44328d55868d5d158e5 🚀

---
+++ Task_1_2_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"graph"},{"l":"graph.algorithm"},{"l":"graph.core"},{"l":"graph.readcore"},{"l":"graph.simple"}];updateSearchResults();
